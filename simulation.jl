614adc5a67f05363c4ca357ed23992cacd314932
Evan Yang <evan.yang@uky.edu>
2019-10-29 14:55:39 -0400

    added selection coefficients and partially implmented switch rate mutation

--- simulation.jl
+++ simulation.jl
@@ -2,12 +2,10 @@
 # Plot the mean value of the switching rate
 # 
 
-
 using Distributions
 
 # Individuals of a Population.
 struct Individual
-    age::Int64
     fitness::Float64            # survival
     phenotype::Int64            # 0 or 1
     genotype::Float64           # switching rate 
@@ -18,21 +16,22 @@ struct Individual
     Individual(age, phenotype) = new(age, phenotype, Int64, Float64)
     Individual(age, phenotype, genotype) = new(age, phenotype, genotype, Float64)
     Individual(age, phenotype, genotype, fitness) = new(age, phenotype, genotype, fitness)
-
 end
 
 # Population - collection of Individuals with population-level properties.
 struct Population
-    size::Int64
     env_state::Int64
     env_switch_rate::Int64
     init_switch_rate::Float64
+    selec_coeff_1::Float64
+    selec_coeff_2::Float64
     members::Array{Individual,1}
     members_prev::Array{Individual,1}
 
     # Constructor
     function Population(size::Int64, env_state::Int64, env_switch_rate::Int64, 
-                        init_switch_rate::Float64)
+                        init_switch_rate::Float64, selec_coeff_1::Float64,
+                        selec_coeff_2::Float64)
         members = Array{Individual}(size)
         # Randomly set phenotype to 0 or 1 for each 
         # individual in initial population.
@@ -50,10 +49,6 @@ struct Population
     end
 end
 
-function mutate_switch_rate(offspring::Individual, parent::Individual)
-    # Unsure of how to mutate switching rate...
-end
-
 # Main life cycle function
 function next_gen(pop::Population, mutation_rate::Float64, switch_env::Bool)
     # Save previous population 
@@ -65,8 +60,11 @@ function next_gen(pop::Population, mutation_rate::Float64, switch_env::Bool)
         if pop.members[i].phenotype == pop.env_state
             pop.members[i].fitness = 1
         else
-
-            pop.members[i].fitness = 1 - # should be selection coefficient, not pop.members[i].genotype
+            if pop.members[i].phenotype == 0
+                pop.members[i].fitness = 1 - pop.selec_coeff_1
+            else
+                pop.members[i].fitness = 1 - pop.selec_coeff_0
+            end
         end
         sum += pop.members[i].fitness
     end
@@ -86,7 +84,7 @@ function next_gen(pop::Population, mutation_rate::Float64, switch_env::Bool)
             parent = rand(1:pop.size) # should not be uniform, should sample based on fitness 
             # Do phenotype switching based on parent's switching
             offspring_switch_phenotype = rand(Uniform(0, 1))
-            if offspring_switch_phenotype <= pop.members_prev[parent]).genotype
+            if offspring_switch_phenotype <= pop.members_prev[parent].genotype
                 if pop.members[i].phenotype == 0
                     pop.members[i].phenotype = 1
                 else
@@ -94,9 +92,12 @@ function next_gen(pop::Population, mutation_rate::Float64, switch_env::Bool)
                 end
             end
             # Mutate switching rate using mutation probability
+            # rand(Truncated(Normal(mu, sigma), 0, 1))
+            # sigma should be chosen to be relatively small, say 0.01 or 0.05
+            # mu is the parental switch rate. 
             offspring_mutate_switch_rate = rand(Uniform(0, 1))
             if offspring_mutate_switch_rate <= mutation_rate
-                mutate_switch_rate(pop.members[i], pop.members_prev[parent]) 
+                pop.members[i].genotype = rand(Truncated(Normal(mu, 0.01), 0, 1))
             end
         end
     end
@@ -112,9 +113,9 @@ function next_gen(pop::Population, mutation_rate::Float64, switch_env::Bool)
 end 
 
 # Main function
-# need to pass in two selection coefficient
 function run_sim(num_generations::Int64, popSize::Int64, env_switch_rate::Float64, 
-                 init_switch_rate::Float64, mutation_rate::Float64)
+                 init_switch_rate::Float64, mutation_rate::Float64, 
+                 selec_coeff_1::Float64, selec_coeff_2::Float64)
     pop = Population(popSize, 0, env_switch_rate, init_switch_rate)
     # Switch the environmental state every num_generations
     for i=1:num_generations
